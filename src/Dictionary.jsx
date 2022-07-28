f185d44cc815969bd239ecc9beab242d6a6d6707
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-28 14:16:26 +0800

    Added Dictionary API

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,15 +1,22 @@
 import react, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
-  function handleKeyWordChange(event) {
-    setKeyword(event.target.value);
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition.`);
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeyWordChange(event) {
+    setKeyword(event.target.value);
   }
 
   return (
